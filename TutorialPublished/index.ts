6cecd49892bcfe35aedef22b77e5c4059fb0887f
Tomáš Biloš <485153@mail.muni.cz>
2023-05-21 21:03:43 +0200

    send email

--- TutorialPublished/index.ts
+++ TutorialPublished/index.ts
@@ -1,21 +1,41 @@
 import { EmailClient } from '@azure/communication-email';
+import { CosmosClient, Resource } from '@azure/cosmos';
 import { AzureFunction, Context } from '@azure/functions';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const client = new EmailClient(process.env.COMMUNICATION_SERVICE_CONN_STRING);
+interface Tutorial {
+  title: string;
+  description: string;
+}
+
+const emailClient = new EmailClient(
+  process.env.COMMUNICATION_SERVICE_CONN_STRING,
+);
+const cosmosClient = new CosmosClient({
+  endpoint: process.env.COSMOS_ENDPOINT,
+  key: process.env.COSMOS_PRIMARY_KEY,
+});
+const container = cosmosClient.database('tutorials').container('subscribers');
 
 const eventGridTrigger: AzureFunction = async function (
   context: Context,
   eventGridEvent: any,
 ): Promise<void> {
-  const recipients = [];
-  const poller = await client.beginSend({
-    senderAddress: '',
-    content: { subject: 'Tutorial updated', plainText: '' },
+  const { resources } = await container.items.readAll().fetchAll();
+
+  const recipients = resources.map((item: { email: string } & Resource) => {
+    return { address: item.email };
+  });
+  const tutorial = eventGridEvent.data as Tutorial;
+  const message = `Tutorial ${tutorial.title} has been updated. Check it out!`;
+  const poller = await emailClient.beginSend({
+    senderAddress:
+      'donotreply@ee3f88af-8920-46e1-9f2f-e8a1afa2d4bd.azurecomm.net',
+    content: { subject: 'Tutorial updated', plainText: message },
     recipients: {
-      to: [],
+      to: recipients,
     },
   });
   await poller.pollUntilDone();
